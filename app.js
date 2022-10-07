be1c52412da3b427b29e0bc63de27d5d5f556fac
rumanabhat <romanabhat27@gmail.com>
2022-10-07 16:28:22 +0530

    added js file

---
+++ app.js
@@ -0,0 +1,7 @@
+let cart=document.querySelector(".popup")
+console.log(cart)
+let button=document.querySelector(".cart")
+button.onclick(showbutton)
+function showbutton(){
+    cart.style.display="block"
+}
